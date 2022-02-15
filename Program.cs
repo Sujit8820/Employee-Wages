200797d13bc030ed8ec33cc82a6c6d110f2547a4
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-15 10:37:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Employee_Wage;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Wellcome to Employee Wage Program");
+    }
+}
