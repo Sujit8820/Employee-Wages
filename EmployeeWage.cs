ec55cb6c0a1e30976662ffb7b3fc35134ce1c7f8
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-18 14:32:38 +0530

    [Sujit] ADDED: Refactor the code to write a class method to compute employee wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,23 +8,23 @@ namespace Employee_Wages
 {
     class EmployeeWage
     {
+        int workingHrs = 0;
+        int empTotalWage = 0;
+        int MAXDAYS = 20;
+        int MAXHRS = 100;
+        int WAGEPERHRS = 20;
 
-        public static int GetWorkingHrs()
+        public int GetWorkingHrs()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-
-            int workingHrs = 0;
-
 
             //Constants
             const int ABSENT = 0;
             const int FULLTIME = 1;
 
-
             switch (empCheck)
             {
-
                 case ABSENT:
                     workingHrs = 0;
                     break;
@@ -39,8 +39,34 @@ namespace Employee_Wages
             }
 
             return workingHrs;
+        }
+
+        public void GetEmpWage()
+        {
+            int day = 0;
+            int totalWorkingHrs = 0;
+
+            EmployeeWage empWage1 = new EmployeeWage();
+
+            while (day < MAXDAYS && totalWorkingHrs <= MAXHRS)
+            {
+                workingHrs = empWage1.GetWorkingHrs();
+
+                totalWorkingHrs = totalWorkingHrs + workingHrs;
+
+                int empWage = WAGEPERHRS * workingHrs;
+                empTotalWage = empTotalWage + empWage;
+
+                Console.WriteLine($"Employee day {day + 1} wage is {empWage}");
+                day++;
+            }
+
+            Console.WriteLine("\nTotal working hours is " + totalWorkingHrs);
+            Console.WriteLine("Total employee wage is " + empTotalWage);
 
         }
+
     }
 }
+
 
--- Program.cs
+++ Program.cs
@@ -10,27 +10,7 @@ class Program
     {
         Console.WriteLine("Wellcome to Employee Wage Program\n");
 
-        int day = 0;
-        int workingHrs = 0;
-        int totalWorkingHrs = 0;
-        int empTotalWage = 0;
-        int MAXDAYS = 20;
-        int MAXHRS = 100;
-        int WAGEPERHRS = 20;
-
-        while (day < MAXDAYS && workingHrs < MAXHRS)
-        {
-            workingHrs = EmployeeWage.GetWorkingHrs();
-            totalWorkingHrs = totalWorkingHrs + workingHrs;
-
-            int empWage = WAGEPERHRS * workingHrs;
-            empTotalWage = empTotalWage + empWage;
-
-            Console.WriteLine($"Employee day {day + 1} wage is {empWage}");
-            day++;
-        }
-        Console.WriteLine("\nTotal working hours is " + totalWorkingHrs);
-        Console.WriteLine("Total employee wage is " + empTotalWage);
-
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.GetEmpWage();
     }
 }
