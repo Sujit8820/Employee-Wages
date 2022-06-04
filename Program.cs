640aa7a2ce70db9333812efd5ad3544b2aa734ca
Sujit8820 <kadamsujit8820@gmail.com>
2022-06-04 19:40:57 +0530

    New

--- Program.cs
+++ Program.cs
@@ -8,7 +8,18 @@ class Program
 {
     public static void Main(string[] args)
     {
+<<<<<<< Updated upstream
         Console.WriteLine("Wellcome to Employee Wage Program");
         EmployeeWage.EmpCheck();
+=======
+        Console.WriteLine("Wellcome to Employee Wage Program\n");
+
+        EmployeeWageBuilder employeeWageBuilder = new EmployeeWageBuilder();
+        employeeWageBuilder.AddEmployeeInfo("Tata", 20, 100, 20);
+        employeeWageBuilder.AddEmployeeInfo("Dmart", 25, 125, 25);
+        employeeWageBuilder.AddEmployeeInfo("Reliance", 30, 140, 35);
+        employeeWageBuilder.AddEmployeeInfo("BigBazar", 23, 110, 22);
+        employeeWageBuilder.CalWage("Tata");
+>>>>>>> Stashed changes
     }
 }
